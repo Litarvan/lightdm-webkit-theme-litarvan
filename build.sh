1a7dba2173317ea8daa953a7d15b0d145adc2711
Adrien Navratil <adrien1975@live.fr>
2017-05-02 23:34:02 +0200

    Added LICENSE file and updated gitignore for .tar.gz

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pub get
 pub build --mode=debug
-cp index.theme build/web
+cp index.theme LICENSE build/web
 pushd build/web
 tar -zcvf lightdm-webkit-theme-litarvan-$(cat ../../version).tar.gz ./*
 mv *.tar.gz ../../
