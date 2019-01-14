32c0501db0d5a80da29230c43878fff895588a58
Adrien Navratil <adrien1975@live.fr>
2019-01-14 16:45:28 +0100

    Version increment, vulnerabilities fixes

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ echo Building LightDM Webkit theme Litarvan v$(cat version)...
 echo
 
 npm install
+rm -r dist/
 npm run-script build
 pushd dist && tar zcvf ../lightdm-webkit-theme-litarvan-$(cat ../version).tar.gz ./* && popd
