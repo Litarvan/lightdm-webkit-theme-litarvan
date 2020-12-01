abfa3e4592b875c8438d527ea72f124c3a665417
Lucien Iseli <lucien.iseli93@gmail.com>
2020-12-01 10:53:31 +0100

    Awesome and i3 don't have 'wm' in their names on login

--- src/lightdm.js
+++ src/lightdm.js
@@ -41,11 +41,11 @@ if (window.lightdm_debug) {
                 key: 'cinnamon'
             },
             {
-                name: 'i3wm',
+                name: 'i3',
                 key: 'i3'
             },
             {
-                name: 'awesome wm',
+                name: 'awesome',
                 key: 'awesome'
             },
             {
