4cf87c3c387ff1887193cfd08fdedf61e6808ccd
Adrien Navratil <adrien1975@live.fr>
2021-04-20 16:55:39 +0900

    Fixed build script errors

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 echo Building LightDM Webkit theme Litarvan v$(cat version)...
 echo
 
-rm lightdm-webkit-theme-litarvan-$(cat version).tar.gz
-npm install
-rm -r dist/
+rm -f lightdm-webkit-theme-litarvan-$(cat version).tar.gz
+npm ci
+rm -rf dist/
 npm run-script build
 pushd dist && tar zcvf ../lightdm-webkit-theme-litarvan-$(cat ../version).tar.gz ./* && popd
