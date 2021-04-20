9bf9dc2972188ca0a1142c480cae3802061797a0
Adrien Navratil <adrien1975@live.fr>
2021-04-20 19:21:52 +0900

    Moved and updated license

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ rm -f lightdm-webkit-theme-litarvan-$(cat version).tar.gz
 npm ci
 rm -rf dist/
 npm run-script build
+cp LICENSE dist/
 pushd dist && tar zcvf ../lightdm-webkit-theme-litarvan-$(cat ../version).tar.gz ./* && popd
