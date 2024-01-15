12027da1a331b3fd55f643e6192783cf218217a6
Zayn Chen <zaynchen@qq.com>
2024-01-16 02:07:38 +0800

    when settings.user.language is an empty string, do not use it

--- src/translations.js
+++ src/translations.js
@@ -290,7 +290,9 @@ import { settings } from '@/settings'
 function getLocale() {
     // get locale from settings.user.language, which is a language.code, like 'en_US.UTF-8'
     // need more test to check if this is working
-    if (settings.user != undefined && settings.user.language != undefined) {
+    if (settings.user != undefined &&
+        settings.user.language != undefined &&
+        settings.user.language !== '') {
         return settings.user.language.split('.')[0].replace('_', '-');
     }
 
